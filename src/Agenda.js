6a08c0ab3684f57e27793dc2df21d10597a00d79
Keefer Gregoire <keefergregoire@keefers-mbp.lan>
2020-07-03 13:43:49 -0400

    Feed work

--- src/Agenda.js
+++ src/Agenda.js
@@ -4,20 +4,21 @@ class Agenda extends React.Component{
 	constructor(props){
 		super(props);
 		
-		this.times = ['12:00AM', '12:30AM', '1:00AM', '1:30AM', '2:00AM', '2:30AM', '3:00AM', '3:30AM', '4:30AM', '5:00AM', '5:30AM', '6:00AM', '6:30AM', '7:00AM', '7:30AM', '8:00AM', '8:30AM', '9:00AM', '9:30AM', '10:00AM', '10:30AM', '11:00AM', '11:30AM', '12:00PM', '12:30PM', '1:00PM', '1:30PM', '2:00PM', '2:30PM', '3:00PM', '3:30PM', '4:30PM', '5:00PM', '5:30PM', '6:00PM', '6:30PM', '7:00PM', '7:30PM', '8:00PM', '8:30PM', '9:00PM', '9:30PM', '10:00PM', '10:30PM', '11:00PM', '11:30PM' ]
+		this.times = ['12:00AM', '12:30AM', '1:00AM', '1:30AM', '2:00AM', '2:30AM', '3:00AM', '3:30AM', '4:00AM', '4:30AM', '5:00AM', '5:30AM', '6:00AM', '6:30AM', '7:00AM', '7:30AM', '8:00AM', '8:30AM', '9:00AM', '9:30AM', '10:00AM', '10:30AM', '11:00AM', '11:30AM', '12:00PM', '12:30PM', '1:00PM', '1:30PM', '2:00PM', '2:30PM', '3:00PM', '3:30PM', '4:00PM', '4:30PM', '5:00PM', '5:30PM', '6:00PM', '6:30PM', '7:00PM', '7:30PM', '8:00PM', '8:30PM', '9:00PM', '9:30PM', '10:00PM', '10:30PM', '11:00PM', '11:30PM' ]
 		
 		this.itemData = [{
 			name: 'Algebra',
 			location: 'Zurn 101',
-			startTime: '10:00AM',
-			endTime: '11:00AM'
+			startTime: '12:00PM',
+			endTime: '9:00PM'
 		},
 		{
 			name: 'Geometry',
 			location: 'Zurn 101',
 			startTime: '1:30PM',
 			endTime: '11:00PM'
-		}]
+		}
+		]
 	}
 	
 	render(){
@@ -31,13 +32,15 @@ class Agenda extends React.Component{
 				height={findHeightProportion(data.startTime, data.endTime)} 
 				top={findTopPosition(data.startTime)} 
 				name={data.name} 
-				location={data.location} 
+				location={data.location}
+				zIndex={index}
 			/>
 		);
 
 		return(
-			<div className='background'>
+			<div className='background' style={{position: 'relative'}}>
 				<h1>Agenda</h1>
+				<button onClick={() => this.props.handleNewAssignment()} id='add-agenda-item'>+</button>
 				<div className='foreground'>
 					{timeHRs}
 					{AgendaItems}
@@ -50,28 +53,25 @@ class Agenda extends React.Component{
 
 function findTopPosition(start){
 	const startMT = convertToMilitary(start);
+	const timeInDay = 2400;
+	const agendaLength = 48 * 25;
 
-	let time =0;
+	console.log(startMT);
+	const startHour = parseInt((startMT+ '').substring(0, (startMT+'').length-2)) * 100;
+	const startMinute = parseInt((startMT+ '').substring((startMT+ '').length-2, (startMT + '').length));
 
-	let ct = 0;
+	console.log(startHour);
+	console.log(startMinute);
 
-	while(time !== startMT){
-		if((time + '').includes('59')){
-			time-= 59;
-			time+= 99;
-			ct++;
-		}else if((time + '').includes('30')){
-			ct++;
-		}
+	const convertMin = (startMinute/60)*100
 
-		time++;
+	console.log(convertMin);
+
+	if (startHour){
+		return (((startHour+convertMin)/2400)* agendaLength) + 17
+	}else{
+		return ((convertMin/2400)* agendaLength) + 17
 	}
-
-	console.log(ct);
-
-	return ct * 25;
-
-
 }
 
 function findHeightProportion(start, end){
@@ -85,9 +85,11 @@ function convertToMilitary(stdTime){
 	const minute = parseInt(stdTime.substring(stdTime.length-4 ,stdTime.length-2));
 	const hour = parseInt(stdTime.substring(0 ,stdTime.length-5));
 
-	if(stdTime.includes('PM')){
+	if(stdTime.includes('PM') && hour === 12){
+		return (hour*100 + minute)
+	}else if(stdTime.includes('PM')){
 		return (hour * 100 + minute) + 1200;
-	}else if(stdTime.includes('12')){
+	}else if(hour === 12){
 		return minute; 
 	}else{
 		return hour * 100 + minute;
@@ -97,7 +99,7 @@ function convertToMilitary(stdTime){
 
 function AgendaItem(props){
 	return(
-		<div style={{height: props.height, top: props.top}} className='item-container'>
+		<div style={{height: props.height, top: props.top, zIndex: props.zIndex}} className='item-container'>
 			<h2>{props.name}</h2>
 			<h3>{props.location}</h3>
 		</div>
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AssignmentDashboard from './AssignmentDashboard';
 import {NewAssignment} from './AssignmentDashboard';
 import Agenda from './Agenda';
+import Feed from './feed'
 
 
 class App extends React.Component{
@@ -44,23 +45,33 @@ class App extends React.Component{
 		return(
 		    <div className="page-container">
 		      <div className="row" style={this.state.showNewAssignmentForm ? {opacity: .6} : null}>
-		        <div className="col-lg-2 menu">
-		          <ul>
-		            <li>List Item 1</li>
-		            <li>List Item 1</li>
-		            <li>List Item 1</li>
-		            <li>List Item 1</li>
-		            <li>List Item 1</li>
-		            <li>List Item 1</li>
+		        <div className="col-lg-2 menu" style={{padding: 0, height: 'fit-content'}}>
+		          <ul style={{padding: 0, margin: 0}}>
+		          	<li style={{listStyle: 'none'}}>
+		          		<button> 
+							Classes
+		          		</button>
+		          	</li>
+		          	<li>
+		          		<button> 
+							Calendar
+		          		</button>
+		          	</li>
+		          	<li>
+		          		<button> 
+							Message Center
+		          		</button>
+		          	</li>
 		          </ul>
 		        </div>
 		        <div className="col-lg-6 feed-container">
 		          <div className="make-post">
-		            <textarea></textarea>
-		            <button>Submit</button>
+		            <p><span className='textarea' role='textbox' contenteditable='true'></span></p>
+					<button><i class="fas fa-camera-retro"></i></button>
+					<button className='submit'>Submit</button>
 		          </div>
 		          <div className="feed-data">
-		            
+		            <Feed />
 		          </div>
 		        </div>
 		        <div className="col-lg-4 dash">
