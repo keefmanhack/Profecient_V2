c8fc7d1556bb1c5a2860a7580e424067ca7c9b13
Keefer Gregoire <keefergregoire@Keefers-MacBook-Pro.local>
2020-10-06 15:51:51 -0400

    message center bug fix

--- backend/app.js
+++ backend/app.js
@@ -229,54 +229,82 @@ app.delete('/user/:id/messageStream/:message_id', function(req, res){
 			foundUser.messageStreams.pull(req.params.message_id);
 			foundUser.save();
 			res.send('success');
+			// Should add function to check if other user has delete the messageStream, if so the messageStream it's self can be deleted
 		}
 	})
 })
 
-app.post('/users/:id/messageStream').populate('messageStreams').exec(function(req, res){
-	User.findById(req.params.id, function(err, foundUser){
-		if(err){
-			console.log(err);
-		}else{
-			let previousStream = doesStreamExist(foundUser.messageStreams, req.body.communicators);
-			if(previousStream===null){
-				MessageStream.create(req.body, function(err, newMessageStream){
-				if(err){
-					console.log(err);
-				}else{
-					req.body.communicators.forEach(function(id){
-						User.findById(id, function(err, foundUser){
-							foundUser.messageStreams.push(newMessageStream);
-							foundUser.save();
-						})
-				})
-					// THIS IS VERY VERY VERY BROKE
-
-			//still need to add check that message stream doesn't already exist
-			res.send('success');
-		}
-	})
+app.post('/users/:id/messageStream', function(req, res){
+	for(let i =0; i<req.body.communicators.length; i++){
+		const id = req.body.communicators[i];
+		User.findById(id).populate('messageStreams').exec(function(err, foundUser){
+			if(err){
+				console.log(err);
 			}else{
-
+				let previousStream = doesStreamExist(foundUser.messageStreams, req.body.communicators);
+				if(previousStream === null){
+					MessageStream.create(req.body, function(err, newMessageStream){
+						if(err){
+							console.log(err);
+						}else{
+							foundUser.messageStreams.push(newMessageStream);
+							foundUser.save(function(err){
+								if(err){
+									console.log(err);
+								}else{
+									if(i===req.body.communicators.length-1){
+										res.send('success');
+									}
+								}
+							});
+							console.log('Created a new stream for ' + foundUser.name);
+						}
+					})
+				}else{
+					MessageStream.findById(previousStream, function(err, foundStream){
+						if(err){
+							console.log(err);
+						}else{
+							foundStream.sentMessages.push(req.body.sentMessages[0]);
+							foundStream.save(function(err){
+								if(err){
+									console.log(err);
+								}else{
+									if(i===req.body.communicators.length-1){
+										res.send('success');
+									}
+								}
+							});
+							console.log('Found Stream updated')
+						}
+					})
+				}
 			}
-		}
-	})
-	
+		})
+	}
 })
 
 function doesStreamExist(messageStreams, communicators){
-	const communs_copy = communicators;
-	Array.sort(communs_copy);
-	for(let i = 0; i< messageStreams.length; i++){
-		const myMessageComms = Array.sort(messageStreams[i].communicators);
-		for(int j =0; j< messageStreams[i].communicators.length; j++){
-			if(communs_copy[j] !== myMessageComms[j]){
-				return messageStream[i]._id;
+	const newComms = communicators;
+	newComms.sort();
+
+	for(let i =0; i< messageStreams.length; i++){
+		if(messageStreams[i].communicators.length === newComms.length){
+			let found = true;
+			const prevComms = messageStreams[i].communicators.sort();
+
+			for(let j =0; j< prevComms.length; j++){
+				if(prevComms[j] != newComms[j]){
+					found=false;
+				}
+			}
+			if(found){
+				return messageStreams[i]._id;
 			}
 		}
 	}
 
-	return null;
+	return null
 }
 
 app.get('/x', function(req, res){
