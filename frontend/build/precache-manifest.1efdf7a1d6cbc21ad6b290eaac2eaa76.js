15b76f1ab505ffe012710595e1a1083ed52dfced
Keefer Gregoire <keefergregoire@keefers-mbp.lan>
2020-12-22 12:46:30 -0500

    initial

---
+++ frontend/build/precache-manifest.1efdf7a1d6cbc21ad6b290eaac2eaa76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b936a9a4864204e1c6eaf6558d8532f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58470063a9d12031ff96",
+    "url": "/static/css/2.8a31f7d9.chunk.css"
+  },
+  {
+    "revision": "3dc8c617a1cd4477fd48",
+    "url": "/static/css/main.3a2de8e6.chunk.css"
+  },
+  {
+    "revision": "58470063a9d12031ff96",
+    "url": "/static/js/2.0474105f.chunk.js"
+  },
+  {
+    "revision": "8067ddae0fc27c31678b5aa90a495f44",
+    "url": "/static/js/2.0474105f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc8c617a1cd4477fd48",
+    "url": "/static/js/main.6fe53c26.chunk.js"
+  },
+  {
+    "revision": "32588cd9c2c9720b07cf",
+    "url": "/static/js/runtime-main.cb8a3bbb.js"
+  }
+]);
